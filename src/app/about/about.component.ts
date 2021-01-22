6535cc9786b53822fec27bc876e568b6b08a334e
DNechev <49711661+DNechev@users.noreply.github.com>
2021-01-22 22:47:03 +0100

    concatinating observables

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { fromEvent, noop, Observable, timer } from 'rxjs';
+import { concat, fromEvent, noop, Observable, of, timer } from 'rxjs';
 import { interval } from 'rxjs';
 import { count, map } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
@@ -14,5 +14,18 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    // This will never complete and therefore source 2 and 3 values would never be emited
+    // const source1$ = interval(1000);
+
+    const source1$ = of(1, 2 , 3);
+
+    const source2$ = of(4, 5, 6);
+
+    const source3$ = of(7, 8, 9);
+
+    const result = concat(source1$, source2$, source3$);
+
+    result.subscribe(numbers => console.log(numbers));
   }
 }
