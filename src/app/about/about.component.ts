7e0ebc5c742cacd9d553b48ef7380e6b47118b7c
DNechev <49711661+DNechev@users.noreply.github.com>
2021-01-24 14:58:59 +0100

    merge operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { concat, fromEvent, noop, Observable, of, timer } from 'rxjs';
+import { concat, fromEvent, merge, noop, Observable, of, timer } from 'rxjs';
 import { interval } from 'rxjs';
 import { count, map } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
@@ -15,17 +15,33 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
+    //#region concat strategy(waiting for each observable to complete)
+
     // This will never complete and therefore source 2 and 3 values would never be emited
     // const source1$ = interval(1000);
 
-    const source1$ = of(1, 2 , 3);
+    // const source1$ = of(1, 2 , 3);
 
-    const source2$ = of(4, 5, 6);
+    // const source2$ = of(4, 5, 6);
 
-    const source3$ = of(7, 8, 9);
+    // const source3$ = of(7, 8, 9);
 
-    const result = concat(source1$, source2$, source3$);
+    // const result = concat(source1$, source2$, source3$);
 
-    result.subscribe(numbers => console.log(numbers));
+    // result.subscribe(numbers => console.log(numbers));
+
+    //#endregion
+
+    //#region merge strategy (observables are being subscribed to parallely)
+
+    const interval1$ = interval(1000);
+    const interval2$ = interval1$.pipe(
+      map(value => value * 10)
+    );
+
+    const result$ = merge(interval1$, interval2$);
+
+    // result$.subscribe(values => console.log(values));
+    //#endregion
   }
 }
