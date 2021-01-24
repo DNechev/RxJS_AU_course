bf9be8611ccb6c7f164091471a9aa1b0575b6688
DNechev <49711661+DNechev@users.noreply.github.com>
2021-01-24 15:32:06 +0100

    exhaust strategy

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -63,7 +63,10 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     ngAfterViewInit() {
 
-
+      fromEvent(this.saveButton.nativeElement, 'click')
+      .pipe(
+        exhaustMap(() => this.saveCourse(this.form.value))
+      ).subscribe(req => console.log(req));
     }
 
 
