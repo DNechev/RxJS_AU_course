c7fd2519c3c30de59cb8fabce575e6d51678dc9f
DNechev <49711661+DNechev@users.noreply.github.com>
2021-01-26 10:30:37 +0100

    BehaviourSubject

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -12,8 +12,19 @@ import {createHttpObservable} from '../common/util';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
+      const subject = new BehaviorSubject(0);
+      const series1$ = subject.asObservable();
 
+      series1$.subscribe(val => console.log('early sub: ' + val))
 
+      subject.next(1);
+      subject.next(2);
+      subject.next(3);
+      // subject.complete();
+
+      setTimeout(() => {
+        series1$.subscribe(val => console.log('late sub: ' + val));
+      }, 3000);
     }
 
 }
