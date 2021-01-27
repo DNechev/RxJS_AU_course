fa0059dcbf5db1e46d648df74264b8af2c057ded
DNechev <49711661+DNechev@users.noreply.github.com>
2021-01-27 10:18:26 +0100

    saving/ updating via Store, First and Take operators examples

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable, Subject, timer } from "rxjs";
-import { tap, map, shareReplay, retryWhen, delayWhen } from "rxjs/operators";
+import { BehaviorSubject, Observable, of, Subject, timer } from "rxjs";
+import { fromPromise } from "rxjs/internal-compatibility";
+import { tap, map, shareReplay, retryWhen, delayWhen, filter } from "rxjs/operators";
 import { Course } from "../model/course";
 import { createHttpObservable } from "./util";
 
@@ -31,5 +32,36 @@ export class Store{
 
   filterByCategory(category: string){
     return this.courses$.pipe(map(courses => courses.filter(course => course.category == category)));
+  }
+
+  saveCourse(courseId: number, changes: any): Observable<any> {
+    const courses = this.subject.getValue();
+
+    const courseIndex = courses.findIndex(course => course.id == courseId);
+
+    const newCourses = courses.slice();
+
+    newCourses[courseIndex] = {
+      ...courses[courseIndex],
+      ...changes
+    }
+
+    this.subject.next(newCourses);
+
+    return fromPromise(
+      fetch(`/api/courses/${courseId}`, {
+        method: 'PUT',
+        body: JSON.stringify(changes),
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
+    );
+  }
+
+  selectCourseById(courseId: number): Observable<Course> {
+    return this.courses$.pipe(
+      map(courses => courses.find(course => course.id == courseId)), filter(course => !!course)
+    )
   }
 }
